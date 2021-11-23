5048ff1063ccfea905b5b91367fa059a39152c9d
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-11-23 19:32:29 +0800

    followed website

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,27 +2,27 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const CompsFooter = () => (
-  <div id="footer" className="footer-layout">
-    <div className="container-fluid d-flex flex-column flex-lg-row justify-content-between">
+  <div id="footer">
+    <div className="container d-flex flex-column flex-lg-row justify-content-between">
 
       <div>
-        <ul className="list-group">
+        <div className="mb-3 mb-lg-0">
           <div className="list-group-item">About</div>
           <Link to="/" className="list-group-item">About us</Link>
           <Link to="/" className="list-group-item">Contact us</Link>
-        </ul>
+        </div>
       </div>
 
       <div>
-        <ul className="list-group">
+        <div className="list-group px-0 px-lg-3 mb-3 mb-lg-0">
           <div className="list-group-item">Customer service</div>
           <Link to="/" className="list-group-item">FAQ</Link>
           <Link to="/" className="list-group-item">T&C</Link>
-        </ul>
+        </div>
       </div>
 
       <div>
-        <div className="list-group">
+        <div className="mb-1 mb-lg-0">
           <div className="list-group-item">Follow us</div>
           <div id="logo" className="fab fa-facebook-square p-1">
             <span className="fab fa-instagram-square" />
