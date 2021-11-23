7ce1171477c80fdf4a908496ba7044b324116758
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-11-23 20:05:28 +0800

    draft header under nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,20 @@ const PagesHome = () => (
       <h1>Home Page</h1>
       <div><Link to="/another">Another Page</Link></div>
     </header>
+
+    <div className="my-3 d-flex justify-content-around" />
+
+    <div className="container text-center my-3">
+      <div className="row">
+        <div className="col delivery">
+          <p>Earn 1 PulsePoint for every $10</p>
+        </div>
+        <div className="col">
+          <p>Free delivery over $500</p>
+        </div>
+      </div>
+    </div>
+
   </div>
 )
 
