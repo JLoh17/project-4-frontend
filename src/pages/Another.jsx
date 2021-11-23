a1ae066396cb98f76de601067e0b2b9631275d40
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-11-23 15:41:34 +0800

    fixed layout

--- src/pages/Another.jsx
+++ src/pages/Another.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const PagesAnother = () => (
   <div id="pages-another" className="container">
-    <header className="text-center border-bottom my-3">
+    <header className="text-center my-3">
       <h1>Another Page</h1>
       <div><Link to="/">Home Page</Link></div>
     </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const PagesHome = () => (
-  <div id="pages-home" className="container vh-100">
+  <div id="pages-home" className="container">
     <header className="text-center my-3">
       <h1>Home Page</h1>
       <div><Link to="/another">Another Page</Link></div>
