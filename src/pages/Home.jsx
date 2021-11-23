1d5345dcbc111d4d7098241ea6cdde62226e8a52
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-11-23 22:08:55 +0800

    draft homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const PagesHome = () => (
   <div id="pages-home" className="container">
-    <header className="text-center my-3">
-      <h1>Home Page</h1>
-      <div><Link to="/another">Another Page</Link></div>
-    </header>
-
-    <div className="my-3 d-flex justify-content-around" />
-
-    <div className="container text-center my-3">
-      <div className="row">
-        <div className="col delivery">
+    <div className="container text-center p-3">
+      <div className="row my-3">
+        <div className="col d-flex justify-content-center points">
+          <div className="fab fa-pied-piper-pp mr-3" />
           <p>Earn 1 PulsePoint for every $10</p>
         </div>
-        <div className="col">
+        <div className="col d-flex justify-content-center delivery">
+          <div className="fas fa-truck mr-3" />
           <p>Free delivery over $500</p>
         </div>
       </div>
-    </div>
 
+      <h1>Featured Products component</h1>
+      <h1>New Products component</h1>
+
+    </div>
   </div>
 )
 
