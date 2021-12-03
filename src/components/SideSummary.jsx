b5b02bb1cba5f5193233255aa62645a0ddc218fd
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-12-03 18:25:43 +0800

    fixed delivery page

--- src/components/SideSummary.jsx
+++ src/components/SideSummary.jsx
@@ -1,8 +1,38 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 const SideOrderSummary = ({ subTotal, points, grandTotal }) => {
-  useState()
+  if (points == 0) {
+    return (
+      <div id="side-summary">
+        <table className="table col-8">
+          <thead>
+            <tr>
+              <th colSpan="2">Summary</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            <tr>
+              <td>Subtotal</td>
+              <td>${subTotal}</td>
+            </tr>
+            {/* <tr>
+              <td>
+                <div>Less points used:</div>
+                <div className="points">{points}pp</div>
+              </td>
+              <td>-$({points / 5 })</td>
+            </tr> */}
+            <tr className="grand-total">
+              <td>Grand Total</td>
+              <td>${grandTotal}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 
   return (
     <div id="side-summary">
@@ -13,6 +43,7 @@ const SideOrderSummary = ({ subTotal, points, grandTotal }) => {
           </tr>
         </thead>
         <tbody>
+
           <tr>
             <td>Subtotal</td>
             <td>${subTotal}</td>
@@ -22,7 +53,7 @@ const SideOrderSummary = ({ subTotal, points, grandTotal }) => {
               <div>Less points used:</div>
               <div className="points">{points}pp</div>
             </td>
-            <td>${points / 5 }</td>
+            <td>-$({points / 5 })</td>
           </tr>
           <tr className="grand-total">
             <td>Grand Total</td>
