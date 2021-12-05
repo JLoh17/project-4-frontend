302f1977aa2325739d1d38dbe31ce6607455151a
JONATHAN LOH <loh_jonathan17@hotmail.COM>
2021-12-05 13:33:05 +0800

    sunday start

--- src/pages/my/Pointbalance.jsx
+++ src/pages/my/Pointbalance.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react'
-import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-
 import { getPointsIndex } from '@/actions/my/profile/balance'
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
+import Pagination from '@mui/material/Pagination'
 
-const MyPointBalance = ({ myPointBalanceState: { point }, currentUserState: { currentUser }, ...props }) => {
+const MyPointBalance = ({ myPointBalanceState: { point, meta }, currentUserState: { currentUser }, ...props }) => {
   useEffect(() => {
     props.getPointsIndex()
   }, [])
 
   const pointsBalance = (currentUser.pointsBalance).toLocaleString(undefined, { minimumFractionDigits: 0 })
 
+  const page = meta?.page
+  const totalPages = meta?.totalPages
+
+  // TODO - how to change the meta for pagination?
   return (
     <div id="point-balance" className="container p-3">
       <h1 className="my-3">Your point balance is: {pointsBalance} </h1>
@@ -36,6 +40,15 @@ const MyPointBalance = ({ myPointBalanceState: { point }, currentUserState: { cu
         }
         </Tbody>
       </Table>
+      <Pagination
+        className="d-flex justify-content-center"
+        variant="outlined"
+        color="primary"
+        count={totalPages}
+        page={page}
+        showFirstButton="1"
+        showLastButton="1"
+      />
     </div>
   )
 }
